ee8c9fd61b8df4888a5909182c68ee23e94c1613
Victório Lázaro <victorio.lazaro15@gmail.com>
2022-09-03 18:33:19 -0400

    modificações na tela de assuntos

---
+++ lib/pages/subjects/widgets/subject_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const List<String> assuntosList = [
+  'Matemática Básica',
+  'Séries e Sequência',
+  'Equações Diferenciais',
+  'Transformada de Laplace' 
+];
+
+const List<String> matBasicaList = [
+  'Resumo: Matemática Básica',
+];
+
+const List<String> seriesSequenciaList = [
+  'Resumo: Séries e Sequência',
+];
+
+const List<String> eqDiferenciaisList = [
+  'Resumo: Equações Diferenciais',
+];
+
+const List<String> transfLaplaceList = [
+  'Resumo: Transformada de Laplace',
+];
--- lib/pages/subjects/widgets/subject_screen.dart
+++ lib/pages/subjects/widgets/subject_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:migoc2/pages/subjects/widgets/subject_button.dart';
 
 class SubjectScreen extends StatefulWidget {
   @override
@@ -6,10 +7,79 @@ class SubjectScreen extends StatefulWidget {
 }
 
 class _SubjectScreen extends State<SubjectScreen> {
+  
+  Card assuntosDropdown(int ordem, List<String> listaAssunto){
+    List<DropdownMenuItem<String>> listaItens = [];
+    listaItens.add(
+        DropdownMenuItem(
+                value: assuntosList[ordem],
+                child: Text(
+                  assuntosList[ordem],
+                  style: const TextStyle(
+                    fontFamily: 'Source Sans Pro', 
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17,
+                  ),
+                ),
+        ),
+    );
+    for(int i = 0; i<listaAssunto.length; i++){
+      listaItens.add(
+        DropdownMenuItem(
+                value: listaAssunto[i],
+                child: Text(
+                  listaAssunto[i],
+                  style: const TextStyle(
+                    color: Color(0xFF39EBB0),
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17,
+                  ),
+                  ),
+        ),
+      );
+    }
+
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: SizedBox(
+        height: 56,
+        width: 344,
+        child: ButtonTheme(
+          alignedDropdown: true,
+          child: DropdownButtonFormField<String>(                        
+            value: assuntosList[ordem],                   
+            decoration: const InputDecoration(
+               prefixIcon: Icon(Icons.keyboard_arrow_down_sharp),
+               enabledBorder:InputBorder.none,
+            ),
+            icon: const Visibility (visible:false, child: Icon(Icons.arrow_downward)),
+            alignment: Alignment.center,
+            items: listaItens,            
+            dropdownColor: const Color(0xFFFFFFFF),
+            borderRadius: BorderRadius.circular(10),
+            onChanged: (value) {                  
+            },
+          ),
+        ),
+      ),
+    );
+  } 
+  
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Assuntos'),
+    return Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            assuntosDropdown(0, matBasicaList),
+            assuntosDropdown(1, seriesSequenciaList),
+            assuntosDropdown(2, eqDiferenciaisList),
+            assuntosDropdown(3, transfLaplaceList),            
+          ],
+        ),
     );
   }
 }
+
+

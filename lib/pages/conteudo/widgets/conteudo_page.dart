cfac7881a045db5e405a8a5042fa46d23ebac47c
Victorio <victorio.lazaro15@gmail.com>
2022-12-15 19:30:14 -0400

    Mais alterações

--- lib/pages/conteudo/widgets/conteudo_page.dart
+++ lib/pages/conteudo/widgets/conteudo_page.dart
@@ -16,64 +16,86 @@ class _ConteudoPage extends State<ConteudoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            widget.conteudo.nome!,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 15,
+              fontFamily: Theme.of(context)
+                .primaryTextTheme
+                .bodyText1
+                ?.fontFamily,
+                color: UIColors.secondaryColor,
+              
+            ),
+          ),
+      ),
       body: Ink(
         child: Center(
-          child: CustomScrollView(
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            slivers: [
-              SliverAppBar.large(
-                title: Text(
-                  widget.conteudo.nome!,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 25,
-                    fontFamily: Theme.of(context)
-                        .primaryTextTheme
-                        .bodyText1
-                        ?.fontFamily,
-                    color: UIColors.secondaryColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'Palavras-Chave:',
+                    ),
                   ),
                 ),
-              ),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  [
-                    Column(
-                      children: [
-                        TextButton(
-                          onPressed: () => {
-                            Navigator.push(context, 
-                              MaterialPageRoute(builder: (context) => const GraficoPage()),)
-                          }, 
-                          child: const Text('Gráfico'),),
-                        SafeArea(
-                          child: TeXView(
-                            renderingEngine: const TeXViewRenderingEngine.katex(),
-                            child: TeXViewDocument(widget.conteudo.corpo!),
-                            style: TeXViewStyle(
-                              backgroundColor: Theme.of(context).backgroundColor,
-                              contentColor: Theme.of(context)
-                                  .primaryTextTheme
-                                  .bodyText1
-                                  ?.color,
-                              padding: const TeXViewPadding.only(
-                                left: 20,
-                                right: 20,
-                              ),
-                              fontStyle: TeXViewFontStyle(
-                                fontSize: 20,
-                                fontFamily: 'Source Sans Pro',
-                                fontWeight: TeXViewFontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10)
                     ),
-                  ],
+                    onPressed: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GraficoPage()),
+                      )
+                    },
+                    child: const Text('Gráfico'),
+                  ),
                 ),
-              ),
-            ],
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GraficoPage()),
+                      )
+                    },
+                    child: const Text('Gráfico'),
+                  ),
+                ),
+                SafeArea(
+                  child: TeXView(
+                    renderingEngine: const TeXViewRenderingEngine.katex(),
+                    child: TeXViewDocument(widget.conteudo.corpo!),
+                    style: TeXViewStyle(
+                      backgroundColor: Theme.of(context).backgroundColor,
+                      contentColor:
+                          Theme.of(context).primaryTextTheme.bodyText1?.color,
+                      padding: const TeXViewPadding.only(
+                        left: 20,
+                        right: 20,
+                      ),
+                      fontStyle: TeXViewFontStyle(
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: TeXViewFontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
